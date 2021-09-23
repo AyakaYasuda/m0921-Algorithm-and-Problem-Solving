dd404fbf83b68a4d18920f3f3a396b138984a409
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-09-23 14:20:56 -0700

    add comments

--- 000_exercise_sorted_challenge.js
+++ 000_exercise_sorted_challenge.js
@@ -10,7 +10,16 @@
 // [5, 4, 3, 2, 1] => true
 // [1, 5, 2] => false
 
-// solution #1
+/*
+solution #1
+*/ 
+
+// 1. If the items in the array are less equal than 2, the result is always true.
+// 2. If not, loop over the array and check 3 items for each, which are previous item, current item and next item.
+// 3. If previous item  is less than current item, and curren item is less than next, too => return true.
+// 4. Also, if previous item is greater than current item, and curren item is greater than next, too => return true.
+// 5. Once the result become false, meaning that items are sorted both ascending and descending, the loop breaks, and the final result will be false. 
+
 function isSorted(arr) {
   let result;
   if (arr.length <= 2) {
@@ -58,12 +67,21 @@ console.log(isSorted([5, 4, 3, 10, 11]));
 
 // empty
 console.log(isSorted([]));
-// no sort
+// same number
 console.log(isSorted([1, 1, 1, 1, 1, 1]));
 
 console.log('--------------------')
 
-// solution #2
+
+
+/* 
+solution #2
+*/
+
+// 1. Create two functions called inAscending and isDescending.
+// 2. In each function, check if one item is less than the next one, greater than the next one.
+// 3. Create the function called isSorted, where both results returned in the two functions are true, or either of them is true, the final result will be true. Otherwise, it will be false. 
+
 const isAscending = function (arr) {
   for (let i = 1; i < arr.length; i++) {
     if (arr[i - 1] < arr[i]) {
